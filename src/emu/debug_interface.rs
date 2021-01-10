f1f40135dafdcce7cdd2ca81602c2bd4b8412296
Seth Pyle <seth.pyle86@gmail.com>
2021-01-10 11:58:32 -0600

    added zero page debug view, need to figure out frame rate stuff

--- src/emu/debug_interface.rs
+++ src/emu/debug_interface.rs
@@ -3,22 +3,53 @@ use std::sync::{Arc, Mutex};
 use crate::emu::bus::Bus;
 
 use std::str;
+use std::time::{Duration, Instant};
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use std::io::{Stdout};
+use termion::raw::{IntoRawMode, RawTerminal};
+use tui::widgets::{Block, Borders, Row, Cell, Table, TableState};
+use tui::layout::{Layout, Constraint};
+use tui::style::{Style, Color, Modifier};
 
 pub struct DebugInterface {
   cpu: Arc<Mutex<CPU>>,
-  bus: Arc<Mutex<Bus>>
+  bus: Arc<Mutex<Bus>>,
+  terminal: Terminal<TermionBackend<RawTerminal<Stdout>>>,
+  state: TableState,
+  frame_duration: u64,
+  last_draw: Instant,
+  pub should_quit: bool
 }
 
 impl DebugInterface {
-  pub fn new(cpu: &Arc<Mutex<CPU>>) -> DebugInterface{
+  pub fn new(cpu: &Arc<Mutex<CPU>>, frame_rate: u32) -> DebugInterface {
+    let stdout = std::io::stdout().into_raw_mode().unwrap();
+    let backend = TermionBackend::new(stdout);
+    let terminal = Terminal::new(backend).unwrap();
+
+    // clear the screen
+    print!("{}[2J", 27 as char);
+
     DebugInterface {
       cpu: cpu.clone(),
-      bus: Arc::clone(&cpu.lock().unwrap().bus)
+      bus: Arc::clone(&cpu.lock().unwrap().bus),
+      terminal,
+      frame_duration: 1000 / frame_rate as u64,
+      state: TableState::default(),
+      last_draw: Instant::now(),
+      should_quit: false
     }
   }
 
-  pub fn test(&self) {
-    println!("{}", self.cpu.lock().unwrap().pc);
+  pub fn draw(&mut self) {
+    let duration = Instant::now().duration_since(self.last_draw);
+
+    if duration.as_millis() < 1000 || self.should_quit == true {
+      return;
+    }
+
+    self.last_draw = Instant::now();
 
     let lock = self.bus.lock().unwrap();
     let ram = lock.ram.to_vec();
@@ -27,6 +58,27 @@ impl DebugInterface {
 
     let formatted = ram_hex.as_bytes().chunks(2).map(str::from_utf8).collect::<Result<Vec<&str>, _>>().unwrap();
 
-    println!("{:?}", &formatted);
+    let mut byte_grid = vec![vec!["00".to_string(); 16]; 16];
+
+    for y in 0..16 {
+      for x in 0..16 {
+        byte_grid[y][x] = formatted[y * 16 + x].to_owned();
+      }
+    }
+
+    self.terminal.draw(|f| {
+      let rects = Layout::default().constraints([Constraint::Percentage(100)].as_ref()).split(f.size());
+      let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+      let normal_style = Style::default().bg(Color::Blue);
+      let header_cells = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B", "0C", "0D", "0E", "0F"].iter().map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+      let header = Row::new(header_cells).style(normal_style).height(1).bottom_margin(1);
+      let rows = byte_grid.iter().map(|row| {
+        let cells = row.iter().map(|r| Cell::from(r.as_str()));
+        Row::new(cells).height(1).bottom_margin(1)
+      });
+      let column_widths = [Constraint::Length(5); 16];
+      let t = Table::new(rows).header(header).block(Block::default().borders(Borders::ALL).title("Zero Page RAM")).highlight_style(selected_style).highlight_symbol(">> ").widths(&column_widths);
+      f.render_widget(t, rects[0]);
+    }).unwrap();
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,21 @@ use std::sync::{Mutex, Arc};
 
 fn main() {
   let bus = Mutex::new(emu::bus::Bus::new());
-  let mut cpu = Arc::new(Mutex::new(CPU::new(bus)));
+  let cpu = Arc::new(Mutex::new(CPU::new(bus)));
 
-  let interface = DebugInterface::new(&cpu);
-  interface.test();
+  let mut interface = DebugInterface::new(&cpu, 60);
 
   cpu.lock().unwrap().pc = 0xFF;
   cpu.lock().unwrap().write(0x0000, 0x69);
+  cpu.lock().unwrap().write(0x0001, 0x70);
+  cpu.lock().unwrap().write(0x0002, 0x71);
+  cpu.lock().unwrap().write(0x00FF, 0x42);
 
-  interface.test();
+  let mut cycles = 0;
+
+  while interface.should_quit != true {
+    interface.draw();
+    cpu.lock().unwrap().bus.lock().unwrap().ram[0x0003] += 1;
+    cycles += 1;
+  }
 }
